639258605b67f495c9eb1b43857425bd6812b46b
Debbie Otuagomah <thatgirldorian@gmail.com>
2022-10-26 22:35:47 +0400

    wired up navigation for back arrow

--- src/components/SelectSourcePage.tsx
+++ src/components/SelectSourcePage.tsx
@@ -1,15 +1,35 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom'
 import ExamplePage from "./ExamplePage"
-import { Typography, Button} from "@material-ui/core";
-import { FixedMiddleBodyWithVerticalScroll, FixedTopBar, PageContainer, TopbarBackButton } from './layout-components';
+import { Typography, Button, Box, IconButton } from "@material-ui/core";
+import {
+    PageContainer,
+    FixedTopBar,
+    FixedMiddleBodyWithVerticalScroll,
+    TopbarBackButton
+} from "./layout-components";
+import _ from "lodash";
 import '../styles/style.css'
 
-const HomePage = () => {
+const SelectSourcePage = () => {
+    const navigate = useNavigate()
+
+    const topbarLeftButton: TopbarBackButton = {
+        type: "back",
+        onClick: () => navigate("/")
+};
 
     return (
-            <ExamplePage />
+        <PageContainer>
+            <FixedTopBar title="Select a source." leftButton={topbarLeftButton} />
+            <FixedMiddleBodyWithVerticalScroll>
+                    <Typography variant="body1" className="select-source-text">
+                        Below is a list of the sources you have connected. Please choose the data source you would like to import data from.
+                    </Typography>
+                </FixedMiddleBodyWithVerticalScroll>
+        </PageContainer>
     )
 
 }
 
-export default HomePage
+export default SelectSourcePage
